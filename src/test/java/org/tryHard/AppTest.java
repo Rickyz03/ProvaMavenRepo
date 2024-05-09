342560e325b177276d349404cea309368719469d
Riccardo Stefani <riccardost02@gmail.com>
2024-05-09 17:18:51 +0200

    Adesso i test dovrebbero essere passati

--- src/test/java/org/tryHard/AppTest.java
+++ src/test/java/org/tryHard/AppTest.java
@@ -1,20 +1,30 @@
 package org.tryHard;
 
-import static org.junit.Assert.assertTrue;
-
+import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testAppOutput() {
+        App.main(new String[]{});
+        assertEquals("Hello World!\n", outContent.toString());
     }
 }
